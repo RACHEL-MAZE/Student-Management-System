b662be6e4c21a009da58b0f5e8cfa9caa0826450
RACHEL-MAZE <georgerachel324@gmail.com>
2026-03-04 12:56:26 +0300

    git input vue page into studentsview

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,13 +6,12 @@
     <input class="mb-3 p-2" v-model="form.age" type="number" min="15" max="40" placeholder="Age" required />
     <input class="mb-3 p-2" v-model="form.email" type="email" placeholder="Email" required />
 
-       <button type="submit" class="text-[#1353aa] hover:text-[#2f81f5]">
-      Save
-    </button>
+       <button type="submit" class="text-[#1353aa] hover:text-[#2f81f5]">Save  </button>
   </form>
 </template>
 
 <script setup>
+import Inputs from './shared/Inputs.vue'
 import { reactive, watch } from 'vue'
 import { toast } from 'vue3-toastify'
 
